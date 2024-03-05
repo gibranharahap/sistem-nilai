afe4428c080abd4f75a72f02135a53e75243d7ea
Gibran <gibranharahap@gmail.com>
2024-03-05 15:25:40 +0700

    feat: 2nd sistem-nilai

--- src/main/java/com/collega/nilai/service/NilaiServiceImpl.java
+++ src/main/java/com/collega/nilai/service/NilaiServiceImpl.java
@@ -1,51 +1,55 @@
 package com.collega.nilai.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.collega.nilai.model.NilaiModel;
+import com.collega.nilai.repository.NilaiDb;
+
+import jakarta.transaction.Transactional;
 
 @Service
+@Transactional
 public class NilaiServiceImpl implements NilaiService {
-    
-    private List<NilaiModel> listNilai;
 
-    public NilaiServiceImpl(){
-        listNilai = new ArrayList<>();
+    @Autowired
+    NilaiDb nilaiDb;
+
+    @Override
+    public List<NilaiModel> getNilaiList() {
+        return nilaiDb.findAll();
     }
 
     @Override
-    public List<NilaiModel> getNilaiList(){
-        return listNilai;
+    public void addNilai(NilaiModel nilai) {
+        nilaiDb.save(nilai);
     }
 
     @Override
-    public void addNilai(NilaiModel nilai){
-        listNilai.add(nilai);
+    public NilaiModel getNilaiById(Long id) {
+        return nilaiDb.findById(id).get();
     }
 
     @Override
-    public NilaiModel getNilaiByNim(String nim){
-        for (NilaiModel temp: listNilai){
-            if(temp.getNim().equals(nim)){
-                return temp;
-            }
+    public NilaiModel updateNilai(NilaiModel nilai) {
+        NilaiModel targetNilai = getNilaiById(nilai.getId());
+        try {
+            targetNilai.setMataKuliah(nilai.getMataKuliah());
+            targetNilai.setNilai(nilai.getNilai());
+            nilaiDb.save(targetNilai);
+            return targetNilai;
+        } catch (NullPointerException nullException) {
+            return null;
         }
-        return null;
     }
 
     @Override
-    public void updateNilai(NilaiModel nilai){
-        NilaiModel targetNilai = getNilaiByNim(nilai.getNim());
-        targetNilai.setNilai(nilai.getNilai());
+    public NilaiModel deleteNilai(Long id) {
+        NilaiModel targetNilai = getNilaiById(id);
+        nilaiDb.delete(targetNilai);
+        return targetNilai;
     }
-
-    @Override
-    public boolean deleteNilaiByNim(String nim){
-        return listNilai.remove(getNilaiByNim(nim));
-    }
-
 
 }
